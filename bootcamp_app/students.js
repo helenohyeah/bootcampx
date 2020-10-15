aaa426487ff10245caf372221c0dc6004a299329
helenohyeah <ouyang.helen@gmail.com>
2020-10-15 01:26:19 +0000

    Modify javascript to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,9 +11,9 @@ pool.query(`
 SELECT students.id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`, [ `%${process.argv[2]}%`, process.argv[3] || 5 ])
 .then(res => {
   res.rows.forEach(user => console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`));
 })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,9 +15,9 @@ FROM teachers
   JOIN assistance_requests ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, [`${process.argv[2]}`])
 .then(res => {
   res.rows.forEach(res => console.log(`${res.cohort}: ${res.teacher}`));
 })
